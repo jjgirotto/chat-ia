9f8d67b1f0ee25691a095ab476d736d521353f29
Juliana Girotto <ads.jjgirotto@gmail.com>
2025-04-29 20:01:03 -0300

    refactor: chatResponseDto

--- src/main/java/com/example/chat/dto/ChatResponseDto.java
+++ src/main/java/com/example/chat/dto/ChatResponseDto.java
@@ -6,6 +6,17 @@ import lombok.Data;
 
 @Data
 public class ChatResponseDto {
-    private List<ChatMessage> answers;
+    private List<Choice> choices;
+
+    public List<ChatMessage> getAnswers() {
+        return choices.stream()
+                .map(Choice::getMessage)
+                .toList();
+    }
+
+    @Data
+    public static class Choice {
+        private ChatMessage message;
+    }
 
 }
